68b41c801a5ddbab1db81c0543384bb32081e027
ssexec <vasnev2016@yandex.ru>
2021-12-01 14:17:19 +0300

    task2.5(small fix)

--- main.py
+++ main.py
@@ -57,10 +57,11 @@ else:
     seti[0]['notes']='need to reset'
     print(seti[0])
 #task 2.5
+
 new_interface={"interface": "Serial0", "ip": "3.3.3.3", "status": "down"}
 seti.append(new_interface)
 seti[2]['ip']='3.3.3.4'
-print(seti)
+print(seti[2])
 #task 2.6
 
 #task 2.7
